57dfed894ef2dae77ec34780c3a8b8fd94113d95
OlexiyVr <olexiyvr@gmail.com>
2023-05-08 22:35:45 +0300

    2

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function onSearch(evt) {
 
 function getWeather(city, days) {
   const BASE_URL = "http://api.weatherapi.com/v1";
-  const API_KEY = "ce2cb9b2a3da414bb5b172546231704";
+  const API_KEY = "8ff74a90545e43e8ad971343230605";
   const params = new URLSearchParams({
     key : API_KEY,
     q: city,
@@ -21,9 +21,7 @@ function getWeather(city, days) {
     lang: 'uk'
   });
 
-  return fetch(
-    `${BASE_URL}/forecast.json?${params}`
-  ).then((resp) => {
+  return fetch(`${BASE_URL}/forecast.json?${params}`).then((resp) => {
     if (!resp.ok) {
       throw new Error(resp.statusText);
     }
